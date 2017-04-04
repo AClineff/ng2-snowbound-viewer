d7777c302fd08fab3bb388d53e2831e43e06126d
Allen <ajclinef@gmail.com>
2017-04-04 12:54:05 -0400

    Add test cases for attributes, events

--- src/components/snowbound-viewer.component.spec.ts
+++ src/components/snowbound-viewer.component.spec.ts
@@ -40,9 +40,73 @@ describe('Test ng2-snowbound-viewer component', () => {
         fixture.detectChanges();
     });
 
-    it('should display hello world', () => {
-        expect(element.querySelector('h1')).toBeDefined();
-        expect(element.getElementsByTagName('h1')[0].innerHTML).toEqual('Hello World Angular 2 test01');
-    });
+    describe('Attributes', ()=>{
+        it('fileNodeId should be mandatory', ()=>{
+            component.showViewer = true;
+            component.fileNodeId = undefined;
+
+            expect(()=>{
+                component.ngOnChanges();
+            }).toThrow();
+        })
+
+        it('If fileNodeId is present, no errors should be thrown', ()=>{
+            component.showViewer = true;
+            component.fileNodeId = '12345';
+
+            expect(()=>{
+                component.ngOnChanges();
+            }).not.toThrow();
+        })
+
+        it('showViewer default value should be true', ()=>{
+            expect(component.showViewer).toBeTruthy();
+        })
+
+        it('overlayMode default value should be false', ()=>{
+            expect(component.overlayMode).toBeFalsy();
+        })
+
+        it('if showViewer is false, viewer should be hidden', ()=>{
+            component.showViewer = false;
+            fixture.detectChanges();
+            expect(element.querySelector('#viewer-main-container')).toBeNull();
+        })
+    })
+
+    describe("Lifecycle", ()=>{
+        beforeEach(()=>{
+            component.fileNodeId = '12345';
+            component.showViewer = true;
+        });
+
+        describe('Closing Viewer', ()=>{
+            it('should clear fileNodeId', ()=>{
+                component.close();
+                fixture.detectChanges();
+                expect(component.fileNodeId).toBeNull();
+            })
+
+            it('should hide the viewer', ()=>{
+                component.close();
+                fixture.detectChanges();
+                expect(component.showViewer).toBeFalsy();
+            })
+
+            it('should emit a close viewer event', (done)=>{
+                component.showViewerChange.subscribe((e)=>{
+                    expect(e).toBeFalsy();
+                    done();
+                })
+                component.close();
+            })
+        })
+
+        it('should clear fileNodeId on destroy', ()=>{
+            component.ngOnDestroy();
+            fixture.detectChanges();
+            expect(component.fileNodeId).toBeNull();
+        })
+    })
 
 });
